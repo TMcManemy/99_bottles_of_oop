0c7901538bfcd77f575ba226f30c6393d17431c4
Tim McManemy <tim.mcmanemy@gmail.com>
2016-09-16 20:08:33 -0500

    4 tests passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,19 @@
 class Bottles
   def verse(number_of_bottles)
     next_number = number_of_bottles - 1
+    foo = number_of_bottles > 1 ? "bottles" : "bottle"
+    bar = number_of_bottles > 1 ? "one" : "it"
+    case 
+    when next_number > 1
+      phrase = "#{next_number} bottles"
+    when next_number == 1
+      phrase = "1 bottle"
+    else
+      phrase = "no more bottles"
+    end
     <<-VERSE
-#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-Take one down and pass it around, #{next_number} #{next_number > 1 ? "bottles" : "bottle"} of beer on the wall.
+#{number_of_bottles} #{foo} of beer on the wall, #{number_of_bottles} #{foo} of beer.
+Take #{bar} down and pass it around, #{phrase} of beer on the wall.
     VERSE
   end
 end
